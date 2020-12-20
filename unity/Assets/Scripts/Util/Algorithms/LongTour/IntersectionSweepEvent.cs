1d802e635cd6706ffa24b9a4a42696972970544f
Ivelin Slavchev <ivelin.slavchev97@gmail.com>
2020-12-20 19:17:31 +0200

    added basic sweep line and sweep event classes for LongTour

---
+++ unity/Assets/Scripts/Util/Algorithms/LongTour/IntersectionSweepEvent.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using Util.Algorithms;
+using Util.Algorithms.Polygon;
+
+namespace Assets.Scripts.Util.Algorithms.LongTour
+{
+    class IntersectionSweepEvent : ISweepEvent<StatusItem>, IComparable<IntersectionSweepEvent>, IEquatable<IntersectionSweepEvent>
+    {
+        public Vector2 Pos => throw new NotImplementedException();
+
+        public StatusItem StatusItem => throw new NotImplementedException();
+
+        public bool IsStart => throw new NotImplementedException();
+
+        public bool IsEnd => throw new NotImplementedException();
+
+        public int CompareTo(IntersectionSweepEvent other)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Equals(IntersectionSweepEvent other)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ unity/Assets/Scripts/Util/Algorithms/LongTour/IntersectionSweepLine.cs
@@ -0,0 +1,26 @@
+﻿using Util.DataStructures.BST;
+using System;
+using System.Collections.Generic;
+
+namespace Util.Algorithms.LongTour
+{
+    public class IntersectionSweepLine<E, T>
+        where E : ISweepEvent<T>, IComparable<E>, IEquatable<E>
+        where T : IComparable<T>, IEquatable<T>
+    {
+
+        public void FindIntersection(IEnumerable<E> items)
+        {
+            SweepLine<E, T> sweepLine = new SweepLine<E, T>();
+            // TODO: sort events
+            sweepLine.InitializeEvents(items);
+            sweepLine.VerticalSweep(HandleEvent);
+            // TODO: return first intersection?
+        }
+
+        public void HandleEvent(IBST<E> events, IBST<T> status, E ev)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
